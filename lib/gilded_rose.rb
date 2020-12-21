56dc03a4230d93ede8da84362286dbe3ca3fbf1c
Jose Luis Cambero <jlcambero@gmail.com>
2020-12-21 15:07:32 +0100

    refactor

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,36 +8,22 @@ class GildedRose
   end
 
   def update_quality
-    @items.each do |item|
-      next if item.name == SULFURAS
-
-      decrease_sell_in(item)
-      expired = item.sell_in.negative?
-
-      case item.name
-      when AGED_BRIE
-        update_item_quality(item, 1)
-        update_item_quality(item, 1) if expired
-      when BACKSTAGE
-        update_item_quality(item, 1)
-        update_item_quality(item, 1) if item.sell_in < 10
-        update_item_quality(item, 1) if item.sell_in < 5
-        update_item_quality(item, -item.quality) if expired
-      else
-        update_item_quality(item, -1)
-        update_item_quality(item, -1) if expired
-      end
-    end
+    @items.each(&method(:update))
   end
 
   private
 
-  def update_item_quality(item, quantity)
-    item.quality = (item.quality + quantity).clamp(0, 50)
-  end
-
-  def decrease_sell_in(item)
-    item.sell_in = item.sell_in - 1
+  def update(item)
+    case item.name
+    when AGED_BRIE
+      ItemAgedBrieUpdater.new(item).update
+    when BACKSTAGE
+      ItemBackstageUpdater.new(item).update
+    when SULFURAS
+      ItemSulfurasUpdater.new(item).update
+    else
+      ItemUpdater.new(item).update
+    end
   end
 end
 
@@ -54,3 +40,67 @@ class Item
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
+
+class ItemUpdater
+  MAX_QUALITY_ALLOWED = 50
+  MIN_QUALITY_ALLOWED = 0
+
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    update_sell_in
+    update_quality
+    clamp_quality
+  end
+
+  private
+
+  def update_sell_in
+    item.sell_in -= 1
+  end
+
+  def update_quality
+    item.quality += quality_delta
+  end
+
+  def clamp_quality
+    item.quality = item.quality.clamp(MIN_QUALITY_ALLOWED, MAX_QUALITY_ALLOWED)
+  end
+
+  def quality_delta
+    expired? ? -2 : -1
+  end
+
+  def expired?
+    item.sell_in.negative?
+  end
+end
+
+class ItemSulfurasUpdater < ItemUpdater
+  def update; end
+end
+
+class ItemAgedBrieUpdater < ItemUpdater
+  private
+
+  def quality_delta
+    expired? ? 2 : 1
+  end
+end
+
+class ItemBackstageUpdater < ItemUpdater
+  private
+
+  def quality_delta
+    return -item.quality if expired?
+    return 3 if item.sell_in < 5
+    return 2 if item.sell_in < 10
+
+    1
+  end
+end
+
